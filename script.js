e5a90e6ac3c811e13feef877a56771b6f7a5a113
School <kaboy@icloud.com>
2022-03-09 00:15:26 -0600

    added code questions and function layout

---
+++ script.js
@@ -0,0 +1,58 @@
+const codeQuestions = [
+  {
+    question: "This is the first question",
+    choices
+      : ['A', 'B', 'C'],
+    answer: "C. This is the answer." // The answer is C.
+  },
+  {
+    question: "This is the second question",
+    choices
+      : ['A', 'B', 'C'],
+    answer: "A. This is the answer." // The answer is A.
+  },
+  {
+    question: "This is the third question",
+    choices
+      : ['A', 'B', 'C'],
+    answer: "B. This is the answer." // The answer is B.
+  },
+  {
+    question: "This is the fourth question",
+    choices
+      : ['A', 'B', 'C'],
+    answer: "B. This is the answer." // The answer is B.
+  },
+  {
+    question: "This is the fifth question",
+    choices
+      : ['A', 'B', 'C'],
+    answer: "C. This is the answer." // The answer is C.
+  },
+  {
+    question: "This is the sixth question",
+    choices
+      : ['A', 'B', 'C'],
+    answer: "A. This is the answer." // The answer is A.
+  },
+  {
+    question: "This is the seventh question",
+    choices
+      : ['A', 'B', 'C'],
+    answer: "C. This is the answer." // The answer is C.
+  },
+]
+
+function startQuiz() { }
+
+function endQuiz() { }
+
+function saveHighScore() { }
+
+function getHighScore() { }
+
+function clearHighScore() { }
+
+function resetQuiz() { }
+
+//
